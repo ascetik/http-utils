a31434de98523ee0dca5e0fca9d25269dbbab7b6
ascetik <ascetik@free.fr>
2023-11-27 18:04:03 +0100

    HttpServerError implementations

---
+++ src/Exceptions/Server/ServiceUnavailableError.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This is part of the http-utils package
+ *
+ * php version 8.2.3
+ *
+ * @package    Http
+ * @subpackage Utils
+ * @category   HttpServerError
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
+namespace Ascetik\Http\Utils\Exceptions\Server;
+
+use Ascetik\Http\Utils\Types\HttpServerError;
+
+class ServiceUnavailableError extends HttpServerError
+{
+    public function __construct()
+    {
+        $this->message = 'Service Unavailable';
+        $this->code = 503;
+    }
+}
