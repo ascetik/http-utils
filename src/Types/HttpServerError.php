b5b33489d232ad2b83eef5395160ed2e98f984c6
ascetik <ascetik@free.fr>
2023-11-27 18:06:28 +0100

    2 implementations de HttpServerError

--- src/Types/HttpServerError.php
+++ src/Types/HttpServerError.php
@@ -1,9 +1,24 @@
 <?php
 
+/**
+ * This is part of the http-utils package
+ *
+ * php version 8.2.3
+ *
+ * @package    Http
+ * @subpackage Utils
+ * @category   Server Error
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
 namespace Ascetik\Http\Utils\Types;
 
 use Error;
 
-class HttpServerError extends Error
+abstract class HttpServerError extends Error
 {
 }
