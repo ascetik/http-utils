76a050d8519176f8257a9553017a9726b28ff154
ascetik <ascetik@free.fr>
2023-11-27 17:33:32 +0100

    new MethodNotAllowedException

---
+++ src/Exceptions/MethodNotAllowedException.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This is part of the http-utils package
+ *
+ * php version 8.2.3
+ *
+ * @package    Http
+ * @subpackage Utils
+ * @category   Enum
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
+namespace Ascetik\Http\Utils\Exceptions;
+
+use Ascetik\Http\Utils\Types\HttpClientException;
+
+class MethodNotAllowedException extends HttpClientException
+{
+    public function __construct(string $method)
+    {
+        parent::__construct($method . ' method is not allowed', 405);
+    }
+}
